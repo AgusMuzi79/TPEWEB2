54edf7a5c8efb1524b214cd1db9cefe383b66348
AgusMuzi79 <143341926+AgusMuzi79@users.noreply.github.com>
2023-10-17 20:54:38 -0300

    Subo la pagina casi completa
    Falta hacer el router semantico y ver si hay que agregar algo mas al crear una base de datos si no existe

---
+++ TPEWeb2Estilizada/app/controllers/ListaController.php
@@ -0,0 +1,45 @@
+<?php
+
+    require_once './app/views/ListaView.php';
+    require_once './app/models/ListaModel.php';
+    require_once './app/helpers/AuthHelper.php';
+
+    class ListaController {
+        private $model;
+        private $view;
+        private $authHelper;
+
+        public function __construct() {
+            $this->model = new ListaModel();
+            $this->view = new ListaView();
+            $this->authHelper = new AuthHelper();
+
+            $this->authHelper->checkLogged();
+        }
+
+        public function showLista() {
+            $lista = $this->model->getLista();
+            $this->view->showLista($lista);
+        }
+
+        /*
+        public function addLista() {
+
+            // obtengo los datos del usuario
+            $puntaje = $_POST[''];
+            $categoria = $_POST[''];
+    
+            //validaciones
+            if (empty($puntaje) || empty($categoria)) {
+                $this->view->showError("Debe completar todos los campos");
+                return;
+            }
+            
+            $lista = $this->model->addItemLista($categoria, $puntaje);
+            header('Location: ' . BASE_URL);
+        }
+        */
+    }
+    
+
+?>
