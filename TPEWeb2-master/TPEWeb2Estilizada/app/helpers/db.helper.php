dbbd86cc747b18fadca2af749464846ec12686d3
AgusMuzi79 <143341926+AgusMuzi79@users.noreply.github.com>
2023-10-17 21:36:53 -0300

    Delete TPEWeb2-master directory

--- TPEWeb2-master/TPEWeb2Estilizada/app/helpers/db.helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DataBase {
-    public static function crearDB ($host, $username, $password, $databaseName) {
-        $pdo = new PDO ("mysql:host=$host", $username, $password);
-        $query = "CREATE DATABASE IF NOT EXISTS $databaseName";
-        $pdo->exec($query);
-    }
-}
